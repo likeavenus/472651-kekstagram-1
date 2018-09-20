f1cacc1e781a7c110612022cddc2f4b2beb92291
Rafael <rafael_guliev94@list.ru>
2018-09-20 20:57:49 +0300

    travis

--- js/picture.js
+++ js/picture.js
@@ -1,6 +1,5 @@
 'use strict';
 var OBJS = 25;
-var comments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var descriptions = ['Отдыхаем...', 'Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
 var posts = [];
 
@@ -75,7 +74,7 @@ var createListElement = function (comment) {
 
 var renderBigPost = function (post) {
   var bigPostFragment = document.createDocumentFragment();
-  for (var i = 0; i < post.comments.length; i++) {
+  for (i = 0; i < post.comments.length; i++) {
     bigPostFragment.appendChild(createListElement(post.comments[i]));
   }
   commentsList.appendChild(bigPostFragment);
